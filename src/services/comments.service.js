b699d8cbca97f47f7dc7dd3cd46a5fc1f825254f
Manmeet Singh <95163474+manmeet-ms@users.noreply.github.com>
2025-09-12 12:14:57 +0530

    Update comments.service.js

--- src/services/comments.service.js
+++ src/services/comments.service.js
@@ -1,5 +1,5 @@
 import api from "./api.js";
 
 export const postCommentSrv => async (postId, data) = await api.post(`/comments/${postId}`, data, { withCredentials: true });
-export const getCommentCountSrv = async (postId) =>  await api.get(`/comments/count/${postId}`);
-export const getCommentsOnSinglePostSrv = async (postId) =>  await api.get(`/comments/c/${postId}`);
+export const getCommentCountSrv => async (postId) =>  await api.get(`/comments/count/${postId}`);
+export const getCommentsOnSinglePostSrv =>async (postId) =>  await api.get(`/comments/c/${postId}`);
